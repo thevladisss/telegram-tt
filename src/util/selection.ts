d4ac979c05e384a7963a7fb946e9ddaf705b5234
Vladyslav Nosal <vladyslavnosal@gmail.com>
2025-02-03 21:01:46 -0800

    fix redo/undo functions on input in chat

--- src/util/selection.ts
+++ src/util/selection.ts
@@ -4,20 +4,7 @@ export function insertHtmlInSelection(html: string) {
   const selection = window.getSelection();
 
   if (selection?.getRangeAt && selection.rangeCount) {
-    const range = selection.getRangeAt(0);
-    range.deleteContents();
-
-    const fragment = range.createContextualFragment(html);
-    const lastInsertedNode = fragment.lastChild;
-    range.insertNode(fragment);
-    if (lastInsertedNode) {
-      range.setStartAfter(lastInsertedNode);
-      range.setEndAfter(lastInsertedNode);
-    } else {
-      range.collapse(false);
-    }
-    selection.removeAllRanges();
-    selection.addRange(range);
+    document.execCommand('insertHTML', false, html);
   }
 }
 
